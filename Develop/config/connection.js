b6367451c5a1a009b567602e2fd98c63cb50b1b1
Luca Urbanowicz <human.exp11@gmail.com>
2022-03-28 11:44:23 -0600

    updated category-routes js file with CRUD

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -25,14 +25,11 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PW,
     {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
       port: 3306
-    },
     });
 }
 
