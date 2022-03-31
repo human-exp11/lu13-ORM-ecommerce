49258a3a0ad0003443cb277711059af8597dea49
Luca Urbanowicz <human.exp11@gmail.com>
2022-03-30 19:34:27 -0600

    Added video to readme

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -58,7 +58,10 @@ router.get('/:id', (req, res) => {
   });
 });
 
-// create new product
+
+
+
+// this allows the user to create a new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
@@ -68,24 +71,28 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-    Product.create(req.body)
+  Product.create(req.body)
     .then((product) => {
+      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
-        const prodTagIdArray = req.body.tagIds.map((tag_id) => {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
             tag_id,
           };
         });
-        return ProductTag.bulkCreate(prodTagIdArray);
+        return ProductTag.bulkCreate(productTagIdArr);
       }
+      // if no product tags, just respond
       res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
+    });
 });
+
 
 //update product
 router.put('/:id', (req, res) => {
@@ -151,7 +158,6 @@ router.delete('/:id', (req, res) => {
     console.log(err);
     res.status(500).json(err);
   });
-});
 });
 
 router.put("/test", (req, res)=> {
